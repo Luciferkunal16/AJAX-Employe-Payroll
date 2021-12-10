524f2a214866e3dbd35d83cb1663ea01565d5d26
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-12-10 15:38:32 +0530

    Added UC3: retrive data using AJAX Promise

--- src/ajaxEmployee.js
+++ src/ajaxEmployee.js
@@ -5,16 +5,16 @@ function showTime() {
     return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" + date.getSeconds() + "Seconds";
 }
 
-function makeAJAXCall(methodType, url, callback, async = true, data = null) {
+function makeAJAXCall(temp,methodType, url, callback, async = false, data = null) {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
-        console.log(methodType + " State Changed Called At: " + showTime() + " | Ready State: " + xhr.readyState + " | Status: " + xhr.status)
+       // console.log(methodType + " State Changed Called At: " + showTime() + " | Ready State: " + xhr.readyState + " | Status: " + xhr.status)
         if (xhr.readyState === 4) {
             if (xhr.status === 200 || xhr.status === 201) {
-                callback(xhr.responseText)
+                getUserDetails(temp,xhr.responseText)
             }
             else if (xhr.status >= 400) {
-                console.log("\nHandle 400 Client Error OR 500 Server Error At: " + showTime());
+               // console.log("\nHandle 400 Client Error OR 500 Server Error At: " + showTime());
             }
         }
     }
@@ -28,20 +28,35 @@ function makeAJAXCall(methodType, url, callback, async = true, data = null) {
     else {
         xhr.send();
     }
-    console.log(methodType + " Request sent to server at: " + showTime());
+   // console.log(methodType + " Request sent to server at: " + showTime());
 }
 
+const addURL="http://localhost:3000/employee/1"
 
-const deleteURL = "http://localhost:3000/employees/4";
-function userDeleted(data) {
-    console.log("User Deleted At: "+ showTime() + "Data: " + data);
+function getUserDetails(temp ,data) {
+    
+    console.log(temp +" Get User Data: "+data);
 }
-makeAJAXCall("DELETE", deleteURL, userDeleted, true);
-console.log("\nMade DELETE AJAX Call to Server at: " + showTime());
+makeAJAXCall("Str1","GET", addURL, getUserDetails);
+makeAJAXCall("Str3","GET", addURL, getUserDetails);
+makeAJAXCall("Str4","GET", addURL, getUserDetails);
+makeAJAXCall("Str5","GET", addURL, getUserDetails);
+makeAJAXCall("Str6","GET", addURL, getUserDetails);
+makeAJAXCall("Str7","GET", addURL, getUserDetails);
 
-const postURL = "http://localhost:3000/employees";
-const emplData = { "name": "Bakugo", "salary": "100000" };
-function userAdded(data) {
-    console.log("User Added: " + data);
-}
-makeAJAXCall("POST", postURL, userAdded, true, emplData);
+
+
+
+// const deleteURL = "http://localhost:3000/employee/4";
+// function userDeleted(data) {
+//     console.log("User Deleted At: "+ showTime() + "Data: " + data);
+// }
+// makeAJAXCall("DELETE", deleteURL, userDeleted, true);
+// console.log("\nMade DELETE AJAX Call to Server at: " + showTime());
+
+// const postURL = "http://localhost:3000/employee";
+// const emplData = { "name": "Bakugo", "salary": "100000" };
+// function userAdded(data) {
+//     console.log("User Added: " + data);
+// }
+// makeAJAXCall("POST", postURL, userAdded, true, emplData);
